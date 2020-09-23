3358581615dd137acd765c37b7a88441fd2d8b11
francescafornasier <fornasierarmando@mbpdifrancesca.lan>
2020-09-23 02:40:30 +0200

    it works!

--- ig_bot.py
+++ ig_bot.py
@@ -1,5 +1,5 @@
 # ig bot 1
-# modifica automaticamente la bio di instagram ogni giorno
+# Fornasier Francesca
 
 from selenium import webdriver
 import time
@@ -8,13 +8,13 @@ from datetime import datetime
 chromedriverPath = '/Users/fornasierarmando/Documents/chromedriver'
 page_URL = 'https://www.instagram.com'
 browser = webdriver.Chrome(chromedriverPath)
-browser.get(page_URL)   #apre URLq
+browser.get(page_URL)   #apre URL
 
 
 # definizione metodi
 
 def login(browser, username, password):
-    #browser.get("https://www.instagram.com/accounts/login/")
+    browser.get("https://www.instagram.com/accounts/login/")
     time.sleep(2)
 
     try:
@@ -41,7 +41,7 @@ def login(browser, username, password):
         browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[2]/button').click()
         time.sleep(3)
 
-        ok = browser.find_elements_by_xpath('//*[@id="twoFactorErrorAlert"]') :   # if security code is not correct
+        ok = browser.find_elements_by_xpath('//*[@id="twoFactorErrorAlert"]')   # if security code is not correct -> you can try again
         if len(ok)!=0 :
             browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[1]/div/label/input').clear()
             otpNew = input('Security code is not correct.\nInsert security code again: ')
@@ -50,44 +50,36 @@ def login(browser, username, password):
             browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[2]/button').click()
             time.sleep(3)
 
-def editProfileEveryHour(action, bio1, bio2):
+        browser.get('https://www.instagram.com/francesca_fornasier/')
+        time.sleep(3)
+
+
+def editProfileEveryHour(browser, b1, b2):
     birth = datetime(1999,5,16,15,30)
     iterations = 0
 
-    while True:
+    while iterations >= 0 :
         c_time = datetime.now()
         life = c_time - birth
-        life_hours = int(int(life.total_seconds()/3600))
+        life_hours = int(int(life.total_seconds())/3600)
 
-        browser.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[5]/span/img').click()
-        time.sleep(2)
-        browser.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[5]/div[2]/div/div[2]/div[2]/a[3]/div').click()
-        time.sleep(5)
+        browser.get('https://www.instagram.com/accounts/edit/')
+        time.sleep(4)
 
-        #newBio = bio1 + "21 ≈ " + str(int(life_hours/24)) + " ± 1 giorni" + bio2  # ~
-        newBio = bio1 + "21 ≈ " + str(life_hours) + " ± 1 ora" + bio2
+        newBioWithName = updateName(iterations%3) + '\n' + bio1 + "21  ≈  " + str(life_hours) + " ± 1 ore    :)" + bio2
 
-##        if action == "bio":
-##            updateBio(newBio)
-##        else if action == "name":
-##            updateName(iterations%3)
+        editBio(browser, newBioWithName)
 
-        updateBio(newBio)
-        updateName(iterations%3)
-##      if (life_hours%24) == 0 :
-##          updateBio(newBio)
+        time.sleep(4)
+        browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/form/div[10]/div/div/button').click()   # save
 
-        time.sleep(2)
-
-        browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/form/div[10]/div/div/button').click()
-
-        print('Descrizione aggiornata: '+ str(c_time)+'    ||    '+str(int(life_hours/24)))
+        print('Descrizione aggiornata: '+ str(c_time)+'    |    '+updateName(iterations%3)+'    |    '+str(life_hours))
 
         iterations+=1
-        time.sleep(50+60*59)
+        time.sleep(110)
 
 
-def updateBio(new):
+def editBio(browser, new):
     browser.find_element_by_xpath('//*[@id="pepBio"]').clear()
     browser.find_element_by_xpath('//*[@id="pepBio"]').send_keys(new)
 
@@ -96,8 +88,7 @@ def updateName(numL):
     name = ['f', 'r', 'a']
     name[numL] = name[numL].upper()
     newName = str(name[0])+name[1]+name[2]
-    browser.find_element_by_xpath('//*[@id="pepName"]').clear()
-    browser.find_element_by_xpath('//*[@id="pepName"]').send_keys(newName)
+    return newName
 
 
 
@@ -110,8 +101,7 @@ bio2 = "\n________________________________________"
 
 login(browser, username, password)
 
-action = input('What do you want to edit? (choose bw name - bio) ')
-editProfileEveryHour(action, bio1, bio2)
+editProfileEveryHour(browser, bio1, bio2)
 
 time.sleep(2)
 
